fe0b7069a3278766a5dfa170e93f2e79ab4642cd
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-04 16:48:44 +0300

    lesson 127 - styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ShopHeader from "../shop-header";
+import { HomePage, CartPage } from "../pages";
 
 import "./app.css";
-import { Route, Switch } from "react-router-dom";
-import { CartPage, HomePage } from "../pages";
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/cart" component={CartPage} />
-    </Switch>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+
+        <Route path="/cart" component={CartPage} />
+      </Switch>
+    </main>
   );
 };
 
