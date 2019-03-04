de5d45f45f7af5d5547437db3dd2d2a512f5089f
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-04 19:01:24 +0300

    lesson 137 - delete element from array

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,6 +18,27 @@ const booksError = (error) => {
   };
 };
 
+const bookAddedToCart = (bookId) => {
+  return {
+    type: 'BOOK_ADDED_TO_CART',
+    payload: bookId
+  };
+};
+
+const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+const allBooksRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOKS_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
 const fetchBooks = (bookstoreService, dispatch) => () => {
   dispatch(booksRequested());
   bookstoreService
@@ -26,11 +47,9 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
     .catch((err) => dispatch(booksError(err)));
 };
 
-const bookAddedToCart = (id) => {
-  return {
-    type: 'BOOK_ADDED_TO_CART',
-    payload: id
-  };
+export {
+  fetchBooks,
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart
 };
-
-export { fetchBooks, bookAddedToCart };
