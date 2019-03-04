8413ba41b6abd377223e1e6a1ce9c8055375841c
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-04 18:02:26 +0300

    lesson 134 - connect new component to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,63 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 0,
+      name: "Name 0",
+      count: 2,
+      total: 2
+    },
+    {
+      id: 1,
+      name: "Name 1",
+      count: 3,
+      total: 1
+    }
+  ],
+  orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null
       };
+
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
       };
+
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
+      };
+
+    case "BOOK_ADDED_TO_CART":
+      const bookId = action.payload;
+      const book = state.books.find(book => book.id === bookId);
+      const newItem = {
+        id: book.id,
+        name: book.title,
+        count: 1,
+        total: book.price
+      };
+
+      return {
+        ...state,
+        cartItems: [...state.cartItems, newItem]
       };
 
     default:
