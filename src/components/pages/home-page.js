24b8850bdab7693fb87188a79f6c7f54d477106a
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-03-04 16:06:38 +0300

    lesson 125 - read from redux store

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,20 +2,7 @@ import React from "react";
 import BookList from "../book-list";
 
 const HomePage = () => {
-  const books = [
-    {
-      id: 1,
-      title: "Same book",
-      author: "Same author"
-    },
-    {
-      id: 2,
-      title: "Another book",
-      author: "And another author"
-    }
-  ];
-
-  return <BookList books={books} />;
+  return <BookList />;
 };
 
 export default HomePage;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 const initialState = {
-  books: []
+  books: [
+    {
+      id: 1,
+      title: "Same book",
+      author: "Same author"
+    },
+    {
+      id: 2,
+      title: "Another book",
+      author: "And another author"
+    }
+  ]
 };
 
 const reducer = (state = initialState, action) => {
